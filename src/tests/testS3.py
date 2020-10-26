724688843f849cbde967fe70441f704dc3dfe9a4
Jonathan <jonathan.sidotmane@gmail.com>
2020-10-26 15:34:23 +0100

    Overrides the test file contents

--- src/tests/testS3.py
+++ src/tests/testS3.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
-# Taken from https://s3-server.readthedocs.io/en/latest/CLIENTS.html#boto3
 
 import boto3
 
-client = boto3.client(
-    's3',
+s3 = boto3.client('s3', 
     aws_access_key_id='accessKey1',
     aws_secret_access_key='verySecretKey1',
     endpoint_url='http://localhost:8000'
-)
+    )
 
-client.create_bucket(Bucket='my-bucket')
-lists = client.list_buckets()
-
+s3.create_bucket(Bucket="seau")
+print("The current buckets list:")
+print(s3.list_buckets())
