cb3aefe916f4f1bcdb2137e3719ead65ee5c04e1
Jonathan <jonathan.sidotmane@gmail.com>
2020-10-26 15:46:48 +0100

    Pretty prints buckets in the test

--- src/tests/testS3.py
+++ src/tests/testS3.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-
 import boto3
+import pprint
 
-s3 = boto3.client('s3', 
-    aws_access_key_id='accessKey1',
-    aws_secret_access_key='verySecretKey1',
-    endpoint_url='http://localhost:8000'
-    )
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id="accessKey1",
+    aws_secret_access_key="verySecretKey1",
+    endpoint_url="http://localhost:8000",
+)
 
 s3.create_bucket(Bucket="seau")
 print("The current buckets list:")
-print(s3.list_buckets())
+pprint.PrettyPrinter(indent=2).pprint(s3.list_buckets()['Buckets'])
